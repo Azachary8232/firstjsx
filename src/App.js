7302ec9d87d36a8cc2b3f9d8a7c5c048cf9ac1d7
Azachary8232 <azachary8232@hotmail.com>
2022-05-13 17:48:33 -0600

    Prop It Up

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import './App.css';
 
-import MyNewComponent from './components/MyNewComponents';
+import PersonCard from './components/PersonCard';
 
 
 function App() {
   return (
     <div>
-      <h1>Hello Dojo!</h1>
-      <h3>Things I need to do:</h3>
-      <ul>
-        <li>Learn React</li>
-        <li>Climb Mt. Everest</li>
-        <li>Run a marathon</li>
-        <li>Feed the dogs</li>
-      </ul>
-      <MyNewComponent  someText={"Hello World"}/>
-      <MyNewComponent  someText={"Goodbye World"}/>
+      <PersonCard  firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}/>
+      <PersonCard  firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
+      <PersonCard  firstName={"Millard"} lastName={"Fillmore"} age={50} hairColor={"Brown"}/>
+      <PersonCard  firstName={"Maria"} lastName={"Smith"} age={62} hairColor={"Brown"}/>
     </div>
   );
 }
