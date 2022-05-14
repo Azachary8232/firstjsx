f4ed5de02e0a0666d37f10f5ad0ac421661ead44
Azachary8232 <azachary8232@hotmail.com>
2022-05-13 18:56:19 -0600

    Big Inversion

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,28 +1,27 @@
-import React, { Component } from 'react';
 
-class PersonCard extends Component{
+const PersonCard = (props) => {
 
-    constructor(props) {
-        super(props);
-        this.state = {age: this.props.age};
-    }
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {age: this.props.age};
+    // }
 
-    increaseAge = () => {
-        this.setState({age: this.state.age + 1});
-    }
+    // increaseAge = () => {
+    //     this.setState({age: this.state.age + 1});
+    // }
 
 
-    render(){
+
         return(
             <div>
-                <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>{this.state.age}</p>
-                <p>{this.props.hairColor}</p>
-                {this.props.children}
-                <button onClick={ this.increaseAge }>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
+                <h1>{props.lastName}, {props.firstName}</h1>
+                <p>{props.age}</p>
+                <p>{props.hairColor}</p>
+                {props.children}
+                {/* <button onClick={ increaseAge }>Birthday Button for {props.firstName} {this.props.lastName}</button> */}
             </div>
         );
-    }
+
 }
 
 export default PersonCard;
