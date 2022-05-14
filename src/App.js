299bc60ed91667cdabf9a037a09d569746b0ee60
Azachary8232 <azachary8232@hotmail.com>
2022-05-13 18:44:35 -0600

    Putting It Together

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ function App() {
   return (
     <div>
       <PersonCard  firstName={"Jane"} lastName={"Doe"} age={45} hairColor={"Black"}>
-      <p>This is a child</p>
-            <p>This is another child</p>
-            <p>This is even another child</p>
+        <p>This is a child</p>
+        <p>This is another child</p>
+        <p>This is even another child</p>
       </PersonCard>
+      <PersonCard  firstName={"John"} lastName={"Smith"} age={88} hairColor={"Brown"}/>
       <LightSwitch/>
 
     </div>
--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {age: this.props.age};
+    }
+
+    increaseAge = () => {
+        this.setState({age: this.state.age + 1});
+    }
+
+
     render(){
         return(
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
-                <p>{this.props.age}</p>
+                <p>{this.state.age}</p>
                 <p>{this.props.hairColor}</p>
                 {this.props.children}
-                <button onClick={ () => alert("This button has been clicked!")}>Click Me!</button>
+                <button onClick={ this.increaseAge }>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         );
     }
